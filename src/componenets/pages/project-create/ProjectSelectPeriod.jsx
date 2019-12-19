b48070b324aa0b5f81e56c2c77c040cef73dd440
silver23rain <silver23rain@gmail.com>
2019-12-19 19:34:21 +0900

    [Style] input focus, 선택 색상 변경

--- src/componenets/pages/project-create/ProjectSelectPeriod.jsx
+++ src/componenets/pages/project-create/ProjectSelectPeriod.jsx
@@ -2,21 +2,22 @@ import React, { useState } from 'react';
 import { ProjectCreateTemplate } from '../../template';
 
 const ProjectSelectPeriod = () => {
-    // TODO ul 컴포넌트로 분리
-    const [period, setPeriod] = useState(); 
-    const list = [
-      {value: 30 , text:"30 Days"},
-      {value: 90 , text: "3 Months"}, 
-      {value: 180 , text: "6 Months"}];
-    return (
-      <ProjectCreateTemplate
-        pageTitle={`스터디 기간을
+	// TODO ul 컴포넌트로 분리
+	const [period, setPeriod] = useState(); 
+	const list = [
+		{value: 30 , text:"30 Days"},
+		{value: 90 , text: "3 Months"}, 
+		{value: 180 , text: "6 Months"}
+	];
+	return (
+		<ProjectCreateTemplate
+			pageTitle={`스터디 기간을
         알려주세요`}
-        setState={setPeriod}
-        selectList={list}
-        nextLink="/new/perssonal"
-      />
-)
+			setState={setPeriod}
+			selectList={list}
+			nextLink="/new/perssonal"
+		/>
+	)
 };
 
 export default ProjectSelectPeriod;
--- src/componenets/template/ProjectCreateTemplate.jsx
+++ src/componenets/template/ProjectCreateTemplate.jsx
@@ -4,35 +4,45 @@ import { Navigation } from '../organisms/project-create';
 import './ProjectCreate.scss';
 
 const ProjectCreateTemplate = (props) => {
-    const {pageTitle, selectList, setState, prevLink, nextLink} = props;
-    const onSelect = (event) => {
-        const eventTarget = event.target;
-        const {value} = eventTarget;
-        const prevSelected = eventTarget.parentElement.querySelector(".selected");
-        if(prevSelected) {
-            prevSelected.classList.remove("selected")
-        } 
-        eventTarget.classList.add("selected");
-        setState(value);
-    };
-    const lis = selectList.map((item, index) => {
-        return (
-          <li key={item.value} value={item.value} onClick={onSelect}>{item.text}</li>
-        )
-    });
-    lis.push(<li key={0}><input type="number" placeholder="직접입력" /></li>);
-     return (
-       <div className="project-create">
-         <header>
-           <Link to="/">X</Link>
-         </header>
-         <h1>{pageTitle}</h1>
-         <ul>
-           {lis}
-         </ul>
-         <Navigation nextLink={nextLink} prevLink={prevLink} />
-       </div>
-    );
+	const {pageTitle, selectList, setState, prevLink, nextLink} = props;
+	const onSelect = (event) => {
+		const eventTarget = event.target;
+		const {value} = eventTarget;
+		const prevSelected = eventTarget.closest("ul").querySelector(".selected");
+		if(prevSelected) {
+			prevSelected.classList.remove("selected")
+		} 
+		eventTarget.closest("li").classList.add("selected");
+		setState(value);
+	};
+	const onChange = (event) => {
+		const eventTarget = event.target;
+		const {value} = eventTarget;
+		setState(value);
+	}
+   
+	const lis = selectList.map((item) => {
+		return (
+			<li key={item.value} value={item.value} onClick={onSelect}>{item.text}</li>
+		)
+	});
+	lis.push(
+		<li key={0} onClick={onSelect}>
+			<input type="number" pattern="\d*" placeholder="직접입력" onChange={onChange} />
+		</li>
+	);
+	return (
+		<div className="project-create">
+			<header>
+				<Link to="/">X</Link>
+			</header>
+			<h1>{pageTitle}</h1>
+			<ul>
+				{lis}
+			</ul>
+			<Navigation nextLink={nextLink} prevLink={prevLink} />
+		</div>
+	);
 };
 
 export default ProjectCreateTemplate;
