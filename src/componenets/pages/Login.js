819eee632120abe80edd268575abcc6ffbe330f3
TWO-PENCE <toromstar@gmail.com>
2020-01-16 17:23:38 +0900

    checkout 때문에 커밋

--- src/componenets/pages/Login.js
+++ src/componenets/pages/Login.js
@@ -1,11 +1,11 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 // import { Route } from 'react-router-dom';
 // import { Main } from './componenets/pages';
 
 class Login extends Component {
-    render() {
-        return <div>로그인입니다.</div>;
-    }
+  render() {
+    return <div>로그인입니다.</div>;
+  }
 }
 
 export default Login;
